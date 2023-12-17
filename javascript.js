2ae56ead479f033fb97fd84cffe213aaa0290e80
sainté_santana <145043257+henry-2210@users.noreply.github.com>
2023-12-17 12:27:06 +0800

    add ui and button functionality
    -added buttons and and created a display in the html to see what the player and computer chooses every round and added a function to evaluate the scores to alert the winner

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let option1 = "rock";
 let option2 = "paper";
 let option3 = "scissors";
 
-function getComputerChoice() {
+const getComputerChoice = function () {
   let randomInteger = Math.floor(Math.random() * 3);
 
   switch (randomInteger) {
@@ -22,13 +22,11 @@ function getComputerChoice() {
   }
 
   return randomInteger;
-}
+};
 
-// console.log(getComputerChoice());
-
-function playRound(p1, p2) {
-  p1 = p1.toLowerCase();
-  p2 = p2.toLowerCase();
+const playRound = function (p1, p2) {
+  // p1 = p1.toLowerCase();
+  // p2 = p2.toLowerCase();
 
   if (p1 == "rock" && p2 == "scissors") {
     return "You won! Rock beats Scissors";
@@ -41,43 +39,124 @@ function playRound(p1, p2) {
   } else {
     return "GAME OVER!";
   }
+};
+
+// const computerChoice = getComputerChoice();
+
+// let player1 = prompt("Rock, Paper, Scissors?");
+
+// let player2 = getComputerChoice();
+
+// console.log(player2);
+// console.log(playRound(player1, player2));
+
+/*=================================
+          DOM MANIPULATION
+=================================*/
+
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+
+rockBtn.textContent = "ROCK";
+paperBtn.textContent = "PAPER";
+scissorsBtn.textContent = "SCISSORS";
+
+const container = document.querySelector(".container");
+
+container.appendChild(rockBtn);
+container.appendChild(paperBtn);
+container.appendChild(scissorsBtn);
+
+rockBtn.addEventListener("click", () => handlerToBtn("rock"));
+
+paperBtn.addEventListener("click", () => handlerToBtn("paper"));
+
+scissorsBtn.addEventListener("click", () => handlerToBtn("scissors"));
+
+const handlerToBtn = function (choice) {
+  const playerChoice = choice;
+  const computerChoice = getComputerChoice();
+
+  const player = document.querySelector("#player-choice");
+  const computer = document.querySelector("#computer-choice");
+  const result = document.querySelector("#result");
+
+  player.textContent = `Player chose ${playerChoice}`;
+  computer.textContent = `Computer chose ${computerChoice}`;
+
+  const roundResult = playRound(playerChoice, computerChoice);
+
+  const playerPoints = document.querySelector("#player-points");
+  const computerPoints = document.querySelector("#computer-points");
+
+  if (roundResult.includes("You won!")) {
+    playerPointsCounter++;
+  } else if (!roundResult.includes("It's a tie")) {
+    computerPointsCounter++;
+  }
+
+  playerPoints.textContent = playerPointsCounter;
+  computerPoints.textContent = computerPointsCounter;
+  // console.log(roundResult);
+  // result.textContent = playRound(playerChoice, computerChoice);
+
+  checkGameStatus();
+};
+
+let playerPointsCounter = 0;
+let computerPointsCounter = 0;
+
+function checkGameStatus() {
+  if (playerPointsCounter == 5) {
+    alert("Congrats! You beat AI!");
+  } else if (computerPointsCounter == 5) {
+    alert("Oh no! You lost to AI. Big Sadge!");
+  }
 }
-
-let player1 = prompt("Rock, Paper, Scissors?");
-
-let player2 = getComputerChoice();
-
-console.log(player2);
-console.log(playRound(player1, player2));
 
 /*
 
-input:choice from both player
-=============================
+goal: make a counter of the results from playRound function and alert a winner after five wins
+input:player choice , computer choice , function that plays a round of rps
+================================================================
+#play a round of rps
+-run function that plays the round between P and C
+playRound(playerChoice , computerChoice )
+================================================================
+#create variable to store the points of P and C
+-variable = player points
+-variable = computer points
 
-get players' choices=
-player , computer
-=====
-set rules=
-rock wins against scissors but looses against paper
+const playerPoints = 0;
+const computerPoints = 0;
+
+================================================================
+#update points in the variable until 5 points is reached
+-every time player wins; add one point to its counter
+-every time computer wins; add one point to its counter
+
+if player wins = update one point
+if(player == you won){
+  update playerPoints +1
+} else {
+  update computerPoints +1
+}
 
 
+if computer wins = update one point
 
-scissors wins against paper but loses against rock
+================================================================
+#alert the winner
+alert; this or that reached 5 points and won
 
+if(playerPoints == 5){
+  alert('Congrats! You won!)
+}else{
+  alert('GAME OVER')
+}
+================================================================
+output: alert a winner after five wins of rps
 
-
-paper wins against rock but loses against scissors
-
-
-=====
-compare choice to one another=
-
-
-announce winner=
-
-
-=============================
-output:show who wins or tie
 
 */
