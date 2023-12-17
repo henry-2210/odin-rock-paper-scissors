0ec72f0994065760ed4e780b9d481edeaeec8684
sainté_santana <145043257+henry-2210@users.noreply.github.com>
2023-12-17 13:14:43 +0800

    fix merging issues

--- javascript.js
+++ javascript.js
@@ -2,11 +2,7 @@ let option1 = "rock";
 let option2 = "paper";
 let option3 = "scissors";
 
-<<<<<<< HEAD
-function getComputerChoice() {
-=======
 const getComputerChoice = function () {
->>>>>>> rps-ui
   let randomInteger = Math.floor(Math.random() * 3);
 
   switch (randomInteger) {
@@ -26,21 +22,11 @@ const getComputerChoice = function () {
   }
 
   return randomInteger;
-<<<<<<< HEAD
-}
-
-// console.log(getComputerChoice());
-
-function playRound(p1, p2) {
-  p1 = p1.toLowerCase();
-  p2 = p2.toLowerCase();
-=======
 };
 
 const playRound = function (p1, p2) {
   // p1 = p1.toLowerCase();
   // p2 = p2.toLowerCase();
->>>>>>> rps-ui
 
   if (p1 == "rock" && p2 == "scissors") {
     return "You won! Rock beats Scissors";
@@ -53,47 +39,6 @@ const playRound = function (p1, p2) {
   } else {
     return "GAME OVER!";
   }
-<<<<<<< HEAD
-}
-
-let player1 = prompt("Rock, Paper, Scissors?");
-
-let player2 = getComputerChoice();
-
-console.log(player2);
-console.log(playRound(player1, player2));
-
-/*
-
-input:choice from both player
-=============================
-
-get players' choices=
-player , computer
-=====
-set rules=
-rock wins against scissors but looses against paper
-
-
-
-scissors wins against paper but loses against rock
-
-
-
-paper wins against rock but loses against scissors
-
-
-=====
-compare choice to one another=
-
-
-announce winner=
-
-
-=============================
-output:show who wins or tie
-
-=======
 };
 
 // const computerChoice = getComputerChoice();
